7b287449200ae67edb28717ab6b7570539dd1305
chanthri383 <36742658+chanthri383@users.noreply.github.com>
2019-04-11 13:37:20 -0500

    Update hw3.cpp

--- hw3.cpp
+++ hw3.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
-#include <queue> //maybe have a queue for the condition variables
+#include <queue> 
 #include <condition_variable>
+
+pthread_t tid[counter]; //not sure how to set up unknown size of n # of threads
+int counter;
+pthread_mutex_t lock;
 
 using namespace std;
 
@@ -14,28 +18,29 @@ int main()
 	int accessTime = 0;
 	int timeOfActivity = 0;
 	int userNumber = 1;
-	//queue<int> currentG;
-	//queue<int> resourceNum;
-	//queue<int> accessT;
-	//queue<int> user;
-	//int userUsingResource = 0;
-	//int currentResource = 0;
-	//int timeActivity = 0;
-	//int user
-	
-	
+	queue<int> currentG;
+	queue<int> resourceNum;
+	queue<int> accessT;
+	queue<int> user;
+	int userUsingResource = 0;
+	int currentResource = 0;
+	int timeActivity = 0;
+	int user
+	int error; //not sure if this is needed 
+	int i = 0; //for loop initializer
 	
 	while(cin >> currentGroup >> resourceNumber >>
 				accessTime >> timeOfActivity)
 				{
 					
-					user.push(userNumber); //user starts with user 1
 					currentG.push(currentGroup);
 					resouceNum.push(resourceNumber);
 					accessT.push(accessTime);
 					
-					userNumber++;
+					userNumber++; //since there are n users accessing shared resouce 
 				}
+	
+	//does creating each thread happen during input redirection?
 	
 	return 0;
 }
