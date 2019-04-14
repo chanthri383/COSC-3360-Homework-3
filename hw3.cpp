092a72240af79767a4bcc5432c6e254ca59cdb32
chanthri383 <36742658+chanthri383@users.noreply.github.com>
2019-04-14 15:49:59 -0500

    Update hw3.cpp

--- hw3.cpp
+++ hw3.cpp
@@ -3,6 +3,8 @@
 #include <mutex>
 #include <queue> 
 #include <condition_variable>
+#include <sys/wait.h>
+#include <unistd.h>
 
 pthread_mutex_t lock;
 
@@ -26,6 +28,9 @@ int main()
 	int user
 	int error; //not sure if this is needed 
 	int i = 0; //for loop initializer
+	int totalRequest1 = 0;
+	int totalRequest2 = 0;
+	int waitAmount = 0;
 	
 	while(cin >> currentGroup >> resourceNumber >>
 				accessTime >> timeOfActivity)
@@ -42,5 +47,14 @@ int main()
 	//does creating each thread happen during input redirection?
 	pthread_t tid[userNumber]; //because we determine how many users based on input redirection
 	
+	cout << "Total Requests:" << endl;
+	cout << "Group 1: " << totalRequest1 << endl;
+	cout << "Group 2: " << totalRequest2 << endl;
+	cout << endl;
+	
+	cout << "Requests that waited: " << endl;
+	cout << "Due to its group: " << //variable that counts which group waited  or not the first line of text file << endl;
+	cout << "Due to a locked position: " << //variable calculate locked position << endl;
+		
 	return 0;
 }
