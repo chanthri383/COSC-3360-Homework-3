05982e6b9a2b8ad5bc6e7fa76f9f6361dea0a222
Alexander Milligan <alexander.ryan.milligan@gmail.com>
2019-04-14 17:53:37 -0500

    specs down

--- hw3.cpp
+++ hw3.cpp
@@ -1,46 +1,95 @@
 #include <iostream>
-#include <thread>
-#include <mutex>
-#include <queue> 
-#include <condition_variable>
-
-pthread_mutex_t lock;
+#include <queue>
 
 using namespace std;
 
-int main()
+/*
+read in all data at the beginning before doing any work
+	* input comes from input redirection
+	* file format:
+		- first line: which user group starts; values: {1, 2}
+		- the rest of the lines: these are requests
+			* each request contains:
+				- user group number; values: {1, 2}
+				- database position; values: 1 through 10
+				- request arrival time; positive integer values
+				- request processing time; positive integer values
+                - userNumber is implied and perhaps unnecesary
+	* store requests into 2 queues, one for each user group
+		- each element of the queue contains: position, arrival, duration, and perhaps userNumber
+		- order elements in each queue by request arrival time
+
+processing requests:
+    * process all requests from the group that starts before doing the next group
+    * each request is handled by a new thread
+    * request processing is simulated with sleep()
+    * print messages whenever a request:
+        a) arrives at the DBMS
+        b) waits for the first group to finish (only printed for each message in the second group)
+        c) waits for a database position (if it was locked)
+        d) uses a database position
+        e) finishes execution
+    * print whenever the first group finishes and the second group begins
+
+print summary after all requests have been processed
+	* this is when both queues are empty
+	* summary consists of:
+		- total number of requests per group
+		- number of requests waited due to:
+			* the group they belong to
+			* a position that was locked by another user
+
+*/
+
+class Shared {
+	// ...
+};
+
+struct RequestInfo {
+	int userNumber;
+	int position;
+	int arrival;
+	int duration;
+
+	RequestInfo(int u, int p, int a, int d) {
+		userNumber = u;
+		position = p;
+		arrival = a;
+		duration = d;
+	}
+};
+
+class UserGroup {
+  private:
+  	char groupLabel;
+	queue<RequestInfo> requests;
+
+  public:
+	UserGroup(char label) {
+		groupLabel = label;
+	}
+
+	void addRequest(int u, int p, int a, int d) {
+		requests.push(RequestInfo(u, p, a, d));
+	}
+
+	RequestInfo dequeueRequest() {
+		RequestInfo r = requests.front;
+		requests.pop();
+		return r;
+	}
+};
+
+
+int main(int argc, char* argv[])
 {
-	int startingGroup = 0;
-	int currentGroup = 0;
-	int resourceNumber = 0;
-	int accessTime = 0;
-	int timeOfActivity = 0;
-	int userNumber = 1;
-	queue<int> currentG;
-	queue<int> resourceNum;
-	queue<int> accessT;
-	queue<int> user;
-	int userUsingResource = 0;
-	int currentResource = 0;
-	int timeActivity = 0;
-	int user
-	int error; //not sure if this is needed 
-	int i = 0; //for loop initializer
-	
-	while(cin >> currentGroup >> resourceNumber >>
-				accessTime >> timeOfActivity)
-				{
-					
-					currentG.push(currentGroup);
-					resouceNum.push(resourceNumber);
-					accessT.push(accessTime);
-					
-					userNumber++; //since there are n users accessing shared resouce 
-							//also used to determine how many request threads
-				}
-	
-	//does creating each thread happen during input redirection?
-	pthread_t tid[userNumber]; //because we determine how many users based on input redirection
-	
+	UserGroup g1('1'), g2('2');
+
+	// add requests
+
+	// process requests
+
+	// print totals
+
 	return 0;
 }
