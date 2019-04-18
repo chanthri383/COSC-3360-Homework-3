54d3aef0d87f7af1171b12782ac55d7475dfb8e6
Alexander Milligan <alexander.ryan.milligan@gmail.com>
2019-04-18 11:52:35 -0500

    added some basic Shared class functionality

--- hw3.cpp
+++ hw3.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
-#include <queue>
+#include <string>
+#include <sstream>
+#include <semaphore.h>
+#include <pthread.h>
+#include <cstring> // memset
+#include <unistd.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <stdexcept>
 
 using namespace std;
 
@@ -52,17 +64,73 @@ missing:
 
 */
 
-class Shared {
-	// ...
+// contains all variables that are shared between processes
+class Shared
+{
+  private:
+	int shmid;
+
+	struct SharedData {
+		pthread_mutex_t position_mutex[10];
+		pthread_mutex_t print_mutex;
+		int numUsersWaitedForLock;
+		pthread_mutex_t numUsersWaitedForLock_mutex;
+	};
+	SharedData *data;
+
+  public:
+	Shared()
+	{
+		shmid = shmget(56785, sizeof(SharedData), 0600 | IPC_CREAT);
+		data = (SharedData *)shmat(shmid, 0, 0);
+
+		data->numUsersWaitedForLock = 0;
+
+		// need to make sure the mutexes and condition variables are sharable in between processes
+		pthread_mutexattr_t shared_mutexattr;
+		pthread_mutexattr_init(&shared_mutexattr);
+		pthread_mutexattr_setpshared(&shared_mutexattr, PTHREAD_PROCESS_SHARED);
+		pthread_mutex_init(&(data->print_mutex), &shared_mutexattr);
+		pthread_mutex_init(&(data->numUsersWaitedForLock_mutex), &shared_mutexattr);
+	}
+	~Shared()
+	{
+		shmdt(data);
+	}
+
+	void print(string s)
+	{
+		pthread_mutex_lock(&(data->print_mutex));
+		cout << s << endl;
+		cout.flush();
+		pthread_mutex_unlock(&(data->print_mutex));
+	}
+
+	void incrementNumUsersWaitedForLock()
+	{
+		pthread_mutex_lock(&(data->numUsersWaitedForLock_mutex));
+		data->numUsersWaitedForLock++;
+		pthread_mutex_unlock(&(data->numUsersWaitedForLock_mutex));
+	}
+
+	int getNumUsersWaitedForLock()
+	{
+		pthread_mutex_lock(&(data->numUsersWaitedForLock_mutex));
+		int t = data->numUsersWaitedForLock;
+		pthread_mutex_unlock(&(data->numUsersWaitedForLock_mutex));
+		return t;
+	}
 };
 
-struct RequestInfo {
+struct RequestInfo
+{
 	int userNumber;
 	int position;
 	int arrival;
 	int duration;
 
-	RequestInfo(int u, int p, int a, int d) {
+	RequestInfo(int u, int p, int a, int d)
+	{
 		userNumber = u;
 		position = p;
 		arrival = a;
@@ -70,39 +138,45 @@ struct RequestInfo {
 	}
 };
 
-class UserGroup {
+class UserGroup
+{
   private:
-  	int groupNumber;
+	int groupNumber;
 	queue<RequestInfo> requests;
 
   public:
-	UserGroup(char n) {
+	UserGroup(char n)
+	{
 		groupNumber = n;
 	}
 
-	void addRequest(int u, int p, int a, int d) {
+	void addRequest(int u, int p, int a, int d)
+	{
 		requests.push(RequestInfo(u, p, a, d));
 	}
 
-	RequestInfo dequeueRequest() {
+	RequestInfo dequeueRequest()
+	{
 		RequestInfo r = requests.front;
 		requests.pop();
 		return r;
 	}
 };
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	UserGroup g1('1'), g2('2');
 	UserGroup *sg, *eg;
 
 	int startingGroup;
 	cin >> startingGroup;
-	if (startingGroup == 1) {
+	if (startingGroup == 1)
+	{
 		sg = &g1;
 		eg = &g2;
-	} else {
+	}
+	else
+	{
 		sg = &g2;
 		eg = &g1;
 	}
